454cd7ae41beceb21eb24ae4c24c9538defeee76
manybotts <105394368+manybotts@users.noreply.github.com>
2024-02-11 13:29:36 +0300

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -9,7 +9,7 @@ api_id = os.environ.get("API_ID", 5166878)
 api_hash = os.environ.get("API_HASH")
 bot_token = os.environ.get("BOT_TOKEN")
 download_dir = os.environ.get("DOWNLOAD_DIR", "downloads/")
-sudo_users = list(set(int(x) for x in os.environ.get("SUDO_USERS").split()))
+sudo_users = os.environ.get("SUDO_USERS", 762308466)
 
 app = Client(":memory:", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
