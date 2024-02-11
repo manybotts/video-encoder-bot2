964600e86d2aabd2991fa55403ecf33178d09db3
manybotts <105394368+manybotts@users.noreply.github.com>
2024-02-11 13:35:13 +0300

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -6,8 +6,8 @@ if os.path.exists('config.env'):
   load_dotenv('config.env')
 
 api_id = os.environ.get("API_ID", 5166878)
-api_hash = os.environ.get("API_HASH")
-bot_token = os.environ.get("BOT_TOKEN")
+api_hash = os.environ.get("API_HASH", "fdafb41f9a67f40e34a6c67f47730a92")
+bot_token = os.environ.get("BOT_TOKEN", "5870372680:AAENb7lbqL1oFyP2KCQRD3TJQ8Zr-ro2AE8")
 download_dir = os.environ.get("DOWNLOAD_DIR", "downloads/")
 sudo_users = os.environ.get("SUDO_USERS", 762308466)
 
